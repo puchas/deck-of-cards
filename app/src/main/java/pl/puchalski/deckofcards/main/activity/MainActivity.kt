d8b0dfcd18032b89b594aafde24f158c9013aaab
Maciej Puchalski <poohalsky@gmail.com>
2019-09-30 23:09:03 +0200

    first view base

---
+++ app/src/main/java/pl/puchalski/deckofcards/main/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package pl.puchalski.deckofcards.main.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pl.puchalski.deckofcards.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
