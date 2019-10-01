e9a45de21cb8b92478f6ce9de8390d119533c0ce
Maciej Puchalski <poohalsky@gmail.com>
2019-10-01 16:43:39 +0200

    retrofit api for cards

---
+++ app/src/main/java/pl/puchalski/deckofcards/cards/repository/CardsApi.kt
@@ -0,0 +1,18 @@
+package pl.puchalski.deckofcards.cards.repository
+
+import pl.puchalski.deckofcards.cards.model.CardsContainer
+import pl.puchalski.deckofcards.cards.model.Deck
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface CardsApi {
+	@GET("new/shuffle")
+	fun shuffleDeck(@Query("deck_count") deckCount: Int): Deck
+
+	@GET("{deckId}/draw")
+	fun drawCards(
+		@Path("deckId") deckId: String,
+		@Query("count") cardsCount: Int
+	): CardsContainer
+}
