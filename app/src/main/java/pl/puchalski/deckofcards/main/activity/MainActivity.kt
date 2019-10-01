0cd703eadacdbd604a5f41ab4605385a80975073
Maciej Puchalski <poohalsky@gmail.com>
2019-10-01 14:58:50 +0200

    base layout for main activity

--- app/src/main/java/pl/puchalski/deckofcards/main/activity/MainActivity.kt
+++ app/src/main/java/pl/puchalski/deckofcards/main/activity/MainActivity.kt
@@ -1,13 +1,33 @@
 package pl.puchalski.deckofcards.main.activity
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import pl.puchalski.deckofcards.R
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+	private val options = arrayOf(1, 2, 3, 4, 5)
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		setupSpinner()
+		setupPlayButton()
+	}
+
+	private fun setupSpinner() {
+		val spinnerAdapter = ArrayAdapter<Int>(this, android.R.layout.simple_spinner_item, options)
+		spinner_deck_count.adapter = spinnerAdapter
+	}
+
+	private fun setupPlayButton() {
+		btn_play.setOnClickListener {
+			val selectedPosition = spinner_deck_count.selectedItemPosition
+			val deckCount = options[selectedPosition]
+			Toast.makeText(this, "Wybrano $deckCount", Toast.LENGTH_SHORT).show()
+		}
+	}
 }
