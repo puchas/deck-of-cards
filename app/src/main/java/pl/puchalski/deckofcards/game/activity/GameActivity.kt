83aac8292c64ad783ff98b7bdbcb3a07fb01d44c
Maciej Puchalski <poohalsky@gmail.com>
2019-10-01 18:14:41 +0200

    setting up recyclerview

--- app/src/main/java/pl/puchalski/deckofcards/game/activity/GameActivity.kt
+++ app/src/main/java/pl/puchalski/deckofcards/game/activity/GameActivity.kt
@@ -2,9 +2,24 @@ package pl.puchalski.deckofcards.game.activity
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_game.*
+import pl.puchalski.deckofcards.R
+import pl.puchalski.deckofcards.game.adapter.CardsAdapter
 
 class GameActivity : AppCompatActivity() {
+
+	private val adapter by lazy { CardsAdapter() }
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_game)
+		setupRecyclerView()
+	}
 
 	private fun restoreDeckCount(): Int {
 		val deckCount = intent?.extras?.getInt(DECK_COUNT)
@@ -12,6 +27,18 @@ class GameActivity : AppCompatActivity() {
 			throw IllegalArgumentException("Use method startGame to start GameActivity")
 		} else {
 			return deckCount
+		}
+	}
+
+	private fun setupRecyclerView() {
+		val orientation = resources.configuration.orientation
+		rv_cards.let {
+			it.adapter = adapter
+			it.layoutManager = if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+				LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+			} else {
+				GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
+			}
 		}
 	}
 
